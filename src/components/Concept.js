ae8502258de2f6e848d85e5f4e6f3bc6242b8766
rezaregata <52247983+rezaregata@users.noreply.github.com>
2023-10-04 20:45:36 +0700

    Initial commit

---
+++ src/components/Concept.js
@@ -0,0 +1,9 @@
+export default function Concept(props) {
+  return (
+    <li className="concept">
+      <img src={props.image} alt={props.title} />
+      <h2>{props.title}</h2>
+      <p>{props.description}</p>
+    </li>
+  );
+}
